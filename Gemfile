a449dcc50b41fe69c6b70a92588a14f83ff4ef46
Delwyn De Villiers <delwyn.d@gmail.com>
2010-07-12 21:43:28 +0100

    Added Devise

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.0.beta4'
+gem 'devise', '1.1.rc2'
 gem 'mysql'
 
 gem "rails3-generators", :group => :development
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,7 @@ RPress::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  
+  # Devise Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
